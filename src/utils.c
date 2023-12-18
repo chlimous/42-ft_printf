15c3415df64e97f5380e194182df700f0bd33e02
Charles Limousin <contact@charleslimousin.com>
2023-12-18 16:06:13 +0100

    Refactoring

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: chlimous <chlimous@student.42.fr>	    +#+  +:+	   +#+	      */
 /*						  +#+#+#+#+#+	+#+	      */
 /*   Created: 2023/11/24 21:22:35 by chlimous	       #+#    #+#	      */
-/*   Updated: 2023/11/24 21:24:17 by chlimous         ###   ########.fr       */
+/*   Updated: 2023/12/18 16:05:14 by chlimous         ###   ########.fr       */
 /*									      */
 /* ************************************************************************** */
 
@@ -29,9 +29,5 @@ void	ft_putchar_fd(char c, int fd)
 
 void	ft_putstr_fd(char *str, int fd)
 {
-	while (*str)
-	{
-		ft_putchar_fd(*str, fd);
-		str++;
-	}
+	write(fd, str, ft_strlen(str));
 }
